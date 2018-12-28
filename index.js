ac55003cd44381f8c509959788dcedd9bf6bdc26
Alex Zdatny <alexzdatny@gmail.com>
2018-12-28 02:30:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-let pageTemplate = _.template(
-  document.getElementById('page-template').innerHTML
-);
+function createPost() {
+  let pageTemplate = _.template(
+    document.getElementById('page-template').innerHTML
+  );
 
-let postTemplate = _.template(
-  document.getElementById('post-template').innerHTML
-);
+  let postTemplate = _.template(
+    document.getElementById('post-template').innerHTML
+  );
 
-let commentsTemplate = _.template(
-  documement.getElementById('comments-template').innerHTML
-);
+  let commentsTemplate = _.template(
+    document.getElementById('comments-template').innerHTML
+  );
+}
