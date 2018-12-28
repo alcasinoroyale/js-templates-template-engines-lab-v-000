4b0c8615b222cc736634bcc6ed3e570b5f817594
Alex Zdatny <alexzdatny@gmail.com>
2018-12-28 02:46:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,29 @@ function createPost() {
   let title = document.getElementById("postTitle").value;
   let author = document.getElementById("postAuthor").value;
   let body = document.getElementById("postBody").value;
+
+  let main = document.getElementsByTagName('main')[0]
+
+	let pageHTML = pageTemplate();
+	let postHTML = postTemplate({ 'title': title, 'body': body, 'author': author });
+	let commentsHTML = commentsTemplate();
+
+	main.innerHTML += pageHTML;
+
+	let postDiv = document.getElementById('post');
+
+	postDiv.innerHTML += postHTML;
+	postDiv.getElementsByTagName('footer')[0].innerHTML += commentsHTML;
+
+}
+
+function postComment() {
+	let name = document.getElementById('commenterName').value;
+	let text = document.getElementById('commenterText').value;
+
+	let commentTemplate = _.template(document.getElementById('comment-template').innerHTML);
+  let commentHTML = commentTemplate({'comment': text, 'commenter': name})
+  let commentsDiv = document.getElementById('comments');
+
+  commentsDiv.innerHTML += commentHTML
 }
