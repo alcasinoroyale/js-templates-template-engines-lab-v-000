e43da383e9ee62ae714d4d479fc21e1cd179bffb
Alex Zdatny <alexzdatny@gmail.com>
2018-12-28 02:30:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 let pageTemplate = _.template(
-  document.getElementById('page-template').innerHTML;
-)
+  document.getElementById('page-template').innerHTML
+);
 
 let postTemplate = _.template(
-  document.getElementById('post-template').innerHTML;
-)
+  document.getElementById('post-template').innerHTML
+);
+
+let commentsTemplate = _.template(
+  documement.getElementById('comments-template').innerHTML
+);
