5c8170a9de1cc9d2d82388a46f4af911e3cb5dbf
Alex Zdatny <alexzdatny@gmail.com>
2018-12-28 02:35:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 function createPost() {
-  let pageTemplate = _.template(
-    document.getElementById('page-template').innerHTML
-  );
+  let pageTemplate = _.template(document.getElementById('page-template').innerHTML);
+  let postTemplate = _.template(document.getElementById('post-template').innerHTML);
+  let commentsTemplate = _.template(document.getElementById('comments-template').innerHTML);
 
-  let postTemplate = _.template(
-    document.getElementById('post-template').innerHTML
-  );
-
-  let commentsTemplate = _.template(
-    document.getElementById('comments-template').innerHTML
-  );
+  let title = document.getElementById("postTitle").value;
+  let author = document.getElementById("postAuthor").value;
+  let post = document.getElementById("post").value;
 }
